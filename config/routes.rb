7fcf314a8f4cb770c9068e3f32454bef2c911c56
RaptorialThing <ruzal.gabdrahmanoff@yandex.ru>
2022-06-16 11:21:18 +0300

    issue#80 added about us and contacts pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages/about_us', as: :about_us
+  get 'pages/contacts', as: :contacts
   root "welcome#index"
   devise_for :users
 
@@ -14,4 +16,10 @@ Rails.application.routes.draw do
   end
 
   get 'dashboard', to: 'users#dashboard'
+
+  namespace :api do
+    namespace :v1 do
+      post 'login', to: 'sessions#create'
+    end
+  end
 end
